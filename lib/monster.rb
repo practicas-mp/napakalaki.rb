55e80df4279fc1d3f4e4cc2a4f7d60076eb62336
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-03-18 23:00:07 +0100

    Se añade MonsterSet para implementar la funcionalidad de la semana 3

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,5 +1,5 @@
 class Monster
-  def initialize(name, combat_level, bad_consequence, prize)
+  def initialize(name, combat_level, prize, bad_consequence)
     @name = name
     @combat_level = combat_level
     @bad_consequence = bad_consequence
@@ -8,6 +8,10 @@ class Monster
 
   def to_s
     "#{@name} [lvl #{@combat_level}]"
+  end
+
+  def just_level_loss
+    @bad_consequence.just_level_loss
   end
 
   attr_reader :name, :combat_level, :bad_consequence, :prize
